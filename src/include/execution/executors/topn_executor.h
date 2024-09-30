bf91bdbd7918e87f8a52447ec3c018fddb620ff4
2549141519 <2549141519@qq.com>
2024-09-30 21:19:11 +0800

    添加top n

--- src/include/execution/executors/topn_executor.h
+++ src/include/execution/executors/topn_executor.h
@@ -63,5 +63,11 @@ class TopNExecutor : public AbstractExecutor {
   const TopNPlanNode *plan_;
   /** The child executor from which tuples are obtained */
   std::unique_ptr<AbstractExecutor> child_executor_;
+
+  std::vector<std::pair<RID,Tuple>> tuples;
+
+  std::vector<std::pair<RID, Tuple>>::iterator iter_;
+
+  size_t limit = 0;
 };
 }  // namespace bustub
--- src/optimizer/sort_limit_as_topn.cpp
+++ src/optimizer/sort_limit_as_topn.cpp
@@ -1,10 +1,28 @@
 #include "optimizer/optimizer.h"
 
+
 namespace bustub {
 
 auto Optimizer::OptimizeSortLimitAsTopN(const AbstractPlanNodeRef &plan) -> AbstractPlanNodeRef {
   // TODO(student): implement sort + limit -> top N optimizer rule
-  return plan;
+  //TopNPlanNode(SchemaRef output, AbstractPlanNodeRef child,
+  //             std::vector<std::pair<OrderByType, AbstractExpressionRef>> order_bys, std::size_t n)
+  SchemaRef schema_ref = std::make_shared<const Schema>(plan->OutputSchema());
+  auto limit_plan = std::dynamic_pointer_cast<const LimitPlanNode>(plan);
+  if (!limit_plan) {
+    return plan; // 如果转换失败，返回原计划
+  }
+  auto sort_plan = std::dynamic_pointer_cast<const SortPlanNode>(plan->children_[0]);
+  if (!sort_plan) {
+    return plan; // 如果转换失败，返回原计划
+  }
+  auto seq_plan = std::dynamic_pointer_cast<const SeqScanPlanNode>(plan->children_[0]);
+  if (!seq_plan) {
+    return plan; // 如果转换失败，返回原计划
+  }
+  auto res = std::make_shared<TopNPlanNode>(schema_ref,seq_plan, 
+    sort_plan->GetOrderBy(),limit_plan->limit_);
+  return res;
 }
 
 }  // namespace bustub
